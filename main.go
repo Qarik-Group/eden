37f09b556a9750670b19aee8cd15a164f7f8d5a3
Dr Nic Williams <drnicwilliams@gmail.com>
2017-04-06 19:39:18 +1000

    rename file

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	edenconfig "github.com/starkandwayne/eden-cli/config"
 )
 
 func main() {
+	rand.Seed(5000)
 	fmt.Println(edenconfig.BrokerEnv())
 }
