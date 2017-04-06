b77badfbcde23e30a182f12ec0f05b5301efdcef
Dr Nic Williams <drnicwilliams@gmail.com>
2017-04-06 19:51:17 +1000

    fetch catalog and display serice/plans

--- main.go
+++ main.go
@@ -1,13 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
+
+	"github.com/pivotal-cf/brokerapi"
 
 	edenconfig "github.com/starkandwayne/eden-cli/config"
 )
 
 func main() {
 	rand.Seed(5000)
-	fmt.Println(edenconfig.BrokerEnv())
+
+	broker := edenconfig.BrokerEnv()
+
+	client := &http.Client{}
+
+	url := fmt.Sprintf("%s/v2/catalog", broker.URL)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(broker.Username, broker.Password)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	catalogResp := &brokerapi.CatalogResponse{}
+
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(resBody, catalogResp)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, service := range catalogResp.Services {
+		for _, plan := range service.Plans {
+			fmt.Println(service.Name, "-", plan.Name, "-", plan.Description)
+		}
+	}
 }
