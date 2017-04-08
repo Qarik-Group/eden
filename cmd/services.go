78ebc8afd6ccc8351f6918ad0df582b61389a0cc
Dr Nic Williams <drnicwilliams@gmail.com>
2017-04-09 07:41:16 +1000

    services -i UUID|NAME displays just that service instnace

--- cmd/services.go
+++ cmd/services.go
@@ -10,15 +10,38 @@ type ServicesOpts struct {
 
 // Execute is callback from go-flags.Commander interface
 func (c ServicesOpts) Execute(_ []string) (err error) {
+  instanceNameOrID := Opts.InstanceName
+  if instanceNameOrID != "" {
+    return c.showService(instanceNameOrID)
+  }
+  return c.showAllServices()
+}
+
+func (c ServicesOpts) showAllServices() (err error) {
   instances := Opts.config().ServiceInstances()
   for _, inst := range instances {
     bindingName := "n/a"
     if len(inst.Bindings) > 0 {
       bindingName = inst.Bindings[0].Name
     }
-    fmt.Printf("%s\t%s\t%s\t%s", inst.Name,
+    fmt.Printf("%s\t%s\t%s\t%s\n", inst.Name,
       inst.ServiceName, inst.PlanName,
       bindingName)
   }
+  return
+}
+
+func (c ServicesOpts) showService(instanceNameOrID string) (err error) {
+  inst := Opts.config().FindServiceInstance(instanceNameOrID)
+  if inst.ServiceID == "" {
+    return fmt.Errorf("services --instance [NAME|GUID] was not found")
+  }
+  bindingName := "n/a"
+  if len(inst.Bindings) > 0 {
+    bindingName = inst.Bindings[0].Name
+  }
+  fmt.Printf("%s\t%s\t%s\t%s\n", inst.Name,
+    inst.ServiceName, inst.PlanName,
+    bindingName)
   return
 }
