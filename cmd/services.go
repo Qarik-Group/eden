06ecb86391e5a4462ea626869fe7c283fc77ebf4
Dr Nic Williams <drnicwilliams@gmail.com>
2017-04-10 05:22:38 -0700

    use boshtable to display services

--- cmd/services.go
+++ cmd/services.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
-  "fmt"
+	"fmt"
+	"os"
+
+	boshtbl "github.com/cloudfoundry/bosh-cli/ui/table"
 )
 
 // ServicesOpts represents the 'services' command
@@ -10,41 +13,63 @@ type ServicesOpts struct {
 
 // Execute is callback from go-flags.Commander interface
 func (c ServicesOpts) Execute(_ []string) (err error) {
-  instanceNameOrID := Opts.Instance.NameOrID
-  if instanceNameOrID != "" {
-    return c.showService(instanceNameOrID)
-  }
-  return c.showAllServices()
+	instanceNameOrID := Opts.Instance.NameOrID
+	if instanceNameOrID != "" {
+		return c.showService(instanceNameOrID)
+	}
+	return c.showAllServices()
 }
 
 func (c ServicesOpts) showAllServices() (err error) {
-  instances := Opts.config().ServiceInstances()
-  for _, inst := range instances {
-    bindingName := "n/a"
-    if len(inst.Bindings) > 0 {
-      bindingName = inst.Bindings[0].Name
-    }
-    fmt.Printf("%s\t%s\t%s\t%s\t%s\n", inst.Name,
-      inst.ServiceName, inst.PlanName,
-      bindingName, inst.BrokerURL)
-  }
-  return
+	table := boshtbl.Table{
+		Content: "services",
+
+		Header: []boshtbl.Header{
+			boshtbl.NewHeader("Name"),
+			boshtbl.NewHeader("Service Name"),
+			boshtbl.NewHeader("Plan Name"),
+			boshtbl.NewHeader("Binding Name"),
+			boshtbl.NewHeader("Broker URL"),
+		},
+
+		SortBy: []boshtbl.ColumnSort{
+			{Column: 1, Asc: true},
+		},
+	}
+
+	instances := Opts.config().ServiceInstances()
+	for _, inst := range instances {
+		bindingName := "n/a"
+		if len(inst.Bindings) > 0 {
+			bindingName = inst.Bindings[0].Name
+		}
+		table.Rows = append(table.Rows, []boshtbl.Value{
+			boshtbl.NewValueString(inst.Name),
+			boshtbl.NewValueString(inst.ServiceName),
+			boshtbl.NewValueString(inst.PlanName),
+			boshtbl.NewValueString(bindingName),
+			boshtbl.NewValueString(inst.BrokerURL),
+		})
+
+	}
+	table.Print(os.Stdout)
+	return
 }
 
 func (c ServicesOpts) showService(instanceNameOrID string) (err error) {
-  inst := Opts.config().FindServiceInstance(instanceNameOrID)
-  if inst.ServiceID == "" {
-    return fmt.Errorf("services --instance [NAME|GUID] was not found")
-  }
-  fmt.Printf("Instance Name: %s\n", inst.Name)
-  fmt.Printf("Service/Plan:  %s/%s\n", inst.ServiceName, inst.PlanName)
-  if len(inst.Bindings) > 0 {
-    fmt.Println("Bindings:")
-    for _, binding := range inst.Bindings {
-      fmt.Printf("- %s\n", binding.Name)
-    }
-  } else {
-    fmt.Println("No bindings.")
-  }
-  return
+	inst := Opts.config().FindServiceInstance(instanceNameOrID)
+	if inst.ServiceID == "" {
+		return fmt.Errorf("services --instance [NAME|GUID] was not found")
+	}
+	fmt.Printf("Instance Name: %s\n", inst.Name)
+	fmt.Printf("Service/Plan:  %s/%s\n", inst.ServiceName, inst.PlanName)
+	if len(inst.Bindings) > 0 {
+		fmt.Println("Bindings:")
+		for _, binding := range inst.Bindings {
+			fmt.Printf("- %s\n", binding.Name)
+		}
+	} else {
+		fmt.Println("No bindings.")
+	}
+	return
 }
