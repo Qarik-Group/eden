e2b6e67188a62f1c3c59cdf078532fccffb570ef
Xiujiao Gao <gaoxiujiao@gmail.com>
2017-09-06 14:38:32 -0400

    eden without any flag now shows help message

--- main.go
+++ main.go
@@ -14,17 +14,28 @@ var Version = ""
 func main() {
 	rand.Seed(5000)
 
-	if os.Args[1] == "-v" || os.Args[1] == "--version" {
-		if Version == "" {
-			fmt.Printf("eden (development)\n")
-		} else {
-			fmt.Printf("eden v%s\n", Version)
+	if len(os.Args) > 1 {
+		if os.Args[1] == "-v" || os.Args[1] == "--version" {
+			if Version == "" {
+				fmt.Printf("eden (development)\n")
+			} else {
+				fmt.Printf("eden v%s\n", Version)
+			}
+			os.Exit(0)
 		}
-		os.Exit(0)
 	}
+
 	parser := flags.NewParser(&edencmd.Opts, flags.Default)
-	_, err := parser.Parse()
-	if err != nil {
-		os.Exit(1)
+
+	if len(os.Args) == 1 {
+		_, err := parser.ParseArgs([]string{"--help"})
+		if err != nil {
+			os.Exit(1)
+		}
+	} else {
+		_, err := parser.Parse()
+		if err != nil {
+			os.Exit(1)
+		}
 	}
 }
