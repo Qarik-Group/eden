a1035a0e0a13b6123ff4e039b831c05034609b86
James Hunt <james@niftylogic.com>
2018-08-30 09:53:27 -0400

    New catalog --strict flag for stricter validation

--- cmd/catalog.go
+++ cmd/catalog.go
@@ -11,6 +11,7 @@ import (
 
 // CatalogOpts represents the 'catalog' command
 type CatalogOpts struct {
+	Strict bool `long:"strict" description:"Validate the catalog using the same heuristics as Cloud Foundry" env:"EDEN_STRICT"`
 }
 
 // Execute is callback from go-flags.Commander interface
@@ -26,6 +27,27 @@ func (c CatalogOpts) Execute(_ []string) (err error) {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
+	}
+
+	if Opts.Catalog.Strict {
+		errors := make([]error, 0)
+		seen := make(map[string] bool)
+		for _, service := range catalogResp.Services {
+			for _, plan := range service.Plans {
+				if _, exists := seen[plan.ID]; exists {
+					errors = append(errors, fmt.Errorf("Service '%s' Plan '%s' ID '%s' is not unique", service.Name, plan.Name, plan.ID))
+				}
+				seen[plan.ID] = true
+			}
+		}
+
+		if len(errors) != 0 {
+			fmt.Fprintf(os.Stderr, "Catalog validation failed:\n")
+			for _, err := range errors {
+				fmt.Fprintf(os.Stderr, "  - %s\n", err)
+			}
+			os.Exit(1)
+		}
 	}
 
 	if Opts.JSON {
